fd26dd4d922364f7d76834f4944af31140ac9c44
Adam <adam-26@users.noreply.github.com>
2018-03-01 22:44:50 +0100

    fix(code): Clear subscribers
    On error, be sure to clear any hoist subscribers

--- src/index.js
+++ src/index.js
@@ -409,6 +409,9 @@ function createChunkComponent(loadFn, options) {
         res.promise = res.promise
           .then(() => { hoistStatics(); })
           .catch(err => {
+            // on error, clear hoist subscriptions
+            dynamicHoistComponentGetters.splice(0, dynamicHoistComponentGetters.length);
+
             if (throwOnImportError === true) {
               // When pre-loading, any loader errors will be thrown immediately (ie: hoistStatics, timeout options)
               // - hoisting implies use of static methods, which need to be available prior to rendering.
